1fede4e5a40c04208a8d57a10de63b038b561f3f
arunrathod82 <arunrathod8296@gmail.com>
2026-03-15 21:22:13 +0530

    Day 8 - Best Time to Buy and Sell Stock II

---
+++ Arrays/MaxProfitII.java
@@ -0,0 +1,34 @@
+/*
+Problem: Best Time to Buy and Sell Stock II
+Approach: Greedy (sum of all positive price differences)
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+import java.util.*;
+
+public class MaxProfitII {
+
+    public static int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        for(int i = 1; i < prices.length; i++) {
+
+            if(prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1];
+            }
+        }
+
+        return profit;
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices = {7,1,5,3,6,4};
+
+        int result = maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + result);
+    }
+}
