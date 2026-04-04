fe94234ee34b9e2928dbc7a55860ba9a2887a4d6
arunrathod82 <arunrathod8296@gmail.com>
2026-04-04 20:16:40 +0530

    Day 27 - Container With Most Water (Two Pointers)

---
+++ Arrays/ContainerWithMostWater.java
@@ -0,0 +1,44 @@
+/*
+Problem: Container With Most Water
+Platform: LeetCode
+Approach: Two Pointers
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+import java.util.*;
+
+public class ContainerWithMostWater {
+
+    public static int maxArea(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while(left < right) {
+
+            int area = Math.min(height[left], height[right]) * (right - left);
+
+            maxArea = Math.max(maxArea, area);
+
+            if(height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = {1,8,6,2,5,4,8,3,7};
+
+        int result = maxArea(height);
+
+        System.out.println("Maximum Area: " + result);
+    }
+}
