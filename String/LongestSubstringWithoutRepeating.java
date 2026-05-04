81512605b9dfade1a1f97526adf5476118802a7b
arunrathod82 <arunrathod8296@gmail.com>
2026-05-04 23:06:21 +0530

    Day 58 - Longest Substring Without Repeating Characters (Sliding Window)

---
+++ String/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,42 @@
+/*
+Problem: Longest Substring Without Repeating Characters
+Platform: LeetCode
+Approach: Sliding Window + HashSet
+Time Complexity: O(n)
+Space Complexity: O(n)
+*/
+
+import java.util.*;
+
+public class LongestSubstringWithoutRepeating {
+
+    public static int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+
+        for(int right = 0; right < s.length(); right++) {
+
+            while(set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+            set.add(s.charAt(right));
+
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "abcabcbb";
+
+        int result = lengthOfLongestSubstring(s);
+
+        System.out.println("Longest Substring Length: " + result);
+    }
+}
